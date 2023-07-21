c5c03fb4d800f597e89731052504e60bc18a4cc8
binchencoder <c1325791024@163.com>
2023-07-21 23:30:22 +0800

    Update the_list.py

--- basic/the_list.py
+++ basic/the_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# https://blog.csdn.net/bang152101/article/details/105702464
+
 classmates = ['Michael', 'Bob', 'Tracy']
 print(classmates[-1])
 
@@ -23,3 +25,21 @@ print(s)
 # list元素也可以是另一个list
 l = ['python', 'java', ['asp', 'php'], 'scheme']
 print(l)
+
+# 方法1：使用for循环简单结构遍历
+print("遍历方法1: for l1 in l")
+for l1 in l :
+    print(f"遍历list: {l1}")
+
+# 方法2：借用 range() 和 len() 函数遍历
+print("遍历方法1: for i in range(len(l))")
+for i in range(len(l)):
+    print(i+1, l[i])
+
+# 方法3：借用 enumerate() 函数遍历
+for i, l1 in enumerate(l):
+    print(i+1, l1)
+
+# 方法4：借用 iter() 函数遍历
+for l1 in iter(l):
+    print(l1)
