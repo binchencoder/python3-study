2ebb9bc673461b0830704bbfe33538f7647ffacb
binchencoder <c1325791024@163.com>
2025-09-23 20:59:47 +0800

    Update

--- str/test_format.py
+++ str/test_format.py
@@ -5,7 +5,16 @@ def str_format(key):
 def str_f(key):
     return f"pie-gpt_{key}"
 
+def replace_space(text: str):
+    # 过滤掉所有的空格
+    no_spaces = "".join(char for char in text if char != " ")
+
+    return no_spaces
+
 
 if __name__ == "__main__":
     print(str_format("aaa"))
     print(str_f("aaa"))
+
+    print("a a a bc cc   ccc  ".replace(" ", ""))
+    print(replace_space("a a a bc cc   ccc  "))
