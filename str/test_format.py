a9d33446ae9af61d37185ee74041b9c7cddae95d
binchencoder <c1325791024@163.com>
2025-09-24 21:12:10 +0800

    Update test_format.py

--- str/test_format.py
+++ str/test_format.py
@@ -5,11 +5,23 @@ def str_format(key):
 def str_f(key):
     return f"pie-gpt_{key}"
 
+
 def replace_space(text: str):
     # 过滤掉所有的空格
     no_spaces = "".join(char for char in text if char != " ")
 
     return no_spaces
+
+
+def substr(text: str):
+    return text[:3]
+
+
+def format_sheetname(sheetname: str) -> str:
+    if sheetname is None or sheetname.strip() == "":
+        return ""
+
+    return sheetname.replace(" ", "")[:5]
 
 
 if __name__ == "__main__":
@@ -18,3 +30,7 @@ if __name__ == "__main__":
 
     print("a a a bc cc   ccc  ".replace(" ", ""))
     print(replace_space("a a a bc cc   ccc  "))
+
+    print(substr("a a a a bc cc   ccc  "))
+
+    print(format_sheetname("a a a a bc cc   ccc "))
