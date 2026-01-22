e81f1f6518b07329064b8d67bde6b829db7ce537
binchencoder <c1325791024@163.com>
2026-01-22 22:21:37 +0800

    Update

--- os/test_path.py
+++ os/test_path.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 path = "/mnt/work/code/extractor/pdf-extractor/output/Fanning/A.pdf"
@@ -5,3 +6,8 @@ path = "/mnt/work/code/extractor/pdf-extractor/output/Fanning/A.pdf"
 if __name__ == '__main__':
     pdf_path = Path(path)
     print(pdf_path.parent)
+
+    print(pdf_path.stem)
+
+    print(os.path.splitext(pdf_path))
+    print(os.path.split(pdf_path))
