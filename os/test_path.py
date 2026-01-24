1cc22192a9cb3c6b983511aaaf59e863b7fdab27
binchencoder <c1325791024@163.com>
2026-01-24 18:22:51 +0800

    Update

--- os/test_path.py
+++ os/test_path.py
@@ -5,9 +5,11 @@ path = "/mnt/work/code/extractor/pdf-extractor/output/Fanning/A.pdf"
 
 if __name__ == '__main__':
     pdf_path = Path(path)
-    print(pdf_path.parent)
+    print(f"pdf_path.parent = {pdf_path.parent}")
 
-    print(pdf_path.stem)
+    print(f"pdf_path.stem = {pdf_path.stem}")
 
-    print(os.path.splitext(pdf_path))
-    print(os.path.split(pdf_path))
+    print(f"pdf_path.name = {pdf_path.name}")
+
+    print(f"os.path.splitext(pdf_path) = {os.path.splitext(pdf_path)}")
+    print(f"os.path.split(pdf_path) = {os.path.split(pdf_path)} ")
